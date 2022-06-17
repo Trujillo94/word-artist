61a4343f6724d485ed98b96c2620d90e26bb883c
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-17 18:45:47 +0200

    Renamed API stage to prod.

--- main.tf
+++ main.tf
@@ -252,7 +252,7 @@ resource "aws_api_gateway_deployment" "apideploy" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  stage_name  = "test"
+  stage_name  = "prod"
   variables = {
     deployed_at = var.deployed_at
   }
