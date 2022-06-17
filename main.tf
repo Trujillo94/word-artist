1fea27f6f7a72b024fe1560540a248e50a37d7f2
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-17 18:05:08 +0200

    Updated endpoint name.

--- main.tf
+++ main.tf
@@ -164,7 +164,7 @@ resource "aws_api_gateway_rest_api" "sample_api" {
 resource "aws_api_gateway_resource" "sample_resource" {
   rest_api_id = aws_api_gateway_rest_api.sample_api.id
   parent_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
-  path_part   = "sample"
+  path_part   = "slack"
 }
 
 resource "aws_api_gateway_method" "sample_method" {
