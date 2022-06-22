ce85f893617aab599172b5e851f0ef22b180aa02
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-22 21:11:00 +0200

    Terraform: fixing enabling S3 access point to lambda.

--- main.tf
+++ main.tf
@@ -330,12 +330,12 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+resource "aws_s3_bucket_policy" "allow_s3_access" {
   bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+  policy = data.aws_iam_policy_document.allow_s3_access.json
 }
 
-data "aws_iam_policy_document" "allow_access_from_another_account" {
+data "aws_iam_policy_document" "allow_s3_access" {
   statement {
     principals {
       type        = "AWS"
