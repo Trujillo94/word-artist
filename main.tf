386fb3fc6505614841ae5449f2786dbb2c425711
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-22 17:52:19 +0200

    Terraform: fixing enabling S3 access point to lambda.

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "aws_iam_policy" "lambda" {
   policy = data.aws_iam_policy_document.lambda.json
 }
 
-resource "aws_lambda_function" "sample_lambda" {
+resource "aws_lambda_function" "lambda" {
   depends_on = [
     null_resource.ecr_image
   ]
@@ -177,7 +177,7 @@ resource "aws_lambda_function" "sample_lambda" {
 }
 
 # Create an API Gateway REST API
-resource "aws_api_gateway_rest_api" "sample_api" {
+resource "aws_api_gateway_rest_api" "api" {
   name        = "${local.project_name}-api"
   description = "WordArtist REST API."
   endpoint_configuration {
@@ -185,27 +185,27 @@ resource "aws_api_gateway_rest_api" "sample_api" {
   }
 }
 
-resource "aws_api_gateway_resource" "sample_resource" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  parent_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "slack"
 }
 
-resource "aws_api_gateway_method" "sample_method" {
-  rest_api_id   = aws_api_gateway_rest_api.sample_api.id
-  resource_id   = aws_api_gateway_resource.sample_resource.id
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "sample_integration" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  resource_id = aws_api_gateway_resource.sample_resource.id
-  http_method = aws_api_gateway_method.sample_method.http_method
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS"
-  uri                     = aws_lambda_function.sample_lambda.invoke_arn
+  uri                     = aws_lambda_function.lambda.invoke_arn
   timeout_milliseconds    = 29000
 
   request_parameters = {
@@ -223,44 +223,44 @@ resource "aws_api_gateway_integration" "sample_integration" {
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  resource_id = aws_api_gateway_resource.sample_resource.id
-  http_method = aws_api_gateway_method.sample_method.http_method
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
   # response_models = "application/json => Empty"
 }
 
-resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  resource_id = aws_api_gateway_resource.sample_resource.id
-  http_method = aws_api_gateway_method.sample_method.http_method
+resource "aws_api_gateway_integration_response" "api_gateway_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
 }
 
 resource "aws_api_gateway_method" "method_root" {
-  rest_api_id   = aws_api_gateway_rest_api.sample_api.id
-  resource_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_method.method_root.resource_id
   http_method = aws_api_gateway_method.method_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS"
-  uri                     = aws_lambda_function.sample_lambda.invoke_arn
+  uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "apideploy" {
   depends_on = [
-    aws_api_gateway_integration.sample_integration,
+    aws_api_gateway_integration.integration,
     aws_api_gateway_integration.lambda_root
   ]
 
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = local.env_tag
   variables = {
     deployed_at = var.deployed_at
@@ -270,11 +270,11 @@ resource "aws_api_gateway_deployment" "apideploy" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.sample_lambda.function_name
+  function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "${aws_api_gateway_rest_api.sample_api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
 
@@ -286,36 +286,36 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_access_point" "sample_lambda_access_point" {
+resource "aws_s3_access_point" "bucket_lambda_access_point" {
   bucket = aws_s3_bucket.bucket.id
-  name   = "sample_bucket"
+  name   = "${local.project_name}-lambda"
 }
 
-resource "aws_s3control_object_lambda_access_point" "sample_lambda_access_point" {
-  name = "sample_lambda_access_point"
+resource "aws_s3control_object_lambda_access_point" "bucket_lambda_access_point_control_object" {
+  name = "${local.project_name}-lambda"
 
   configuration {
-    supporting_access_point = aws_s3_access_point.sample_lambda_access_point.arn
+    supporting_access_point = aws_s3_access_point.bucket_lambda_access_point.arn
 
     transformation_configuration {
-      actions = ["GetObject", "PutObject"]
+      actions = ["GetObject"]
 
       content_transformation {
         aws_lambda {
-          function_arn = aws_lambda_function.sample_lambda.arn
+          function_arn = aws_lambda_function.lambda.arn
         }
       }
     }
   }
 }
 
-resource "aws_s3_bucket_acl" "sample_bucket_acl" {
+resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
 
 output "lambda_name" {
-  value = aws_lambda_function.sample_lambda.id
+  value = aws_lambda_function.lambda.id
 }
 
 output "base_url" {
