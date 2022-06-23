4b26a01b253d95ba3fb6af5719a49c0e719ba801
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-23 14:54:12 +0200

    Enabled API stage CloudWath logs.

--- main.tf
+++ main.tf
@@ -316,7 +316,8 @@ resource "aws_api_gateway_deployment" "apideploy" {
 resource "aws_api_gateway_stage" "api-stage" {
   deployment_id = aws_api_gateway_deployment.apideploy.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  stage_name    = aws_api_gateway_deployment.apideploy.stage_name
+  # stage_name    = aws_api_gateway_deployment.apideploy.stage_name
+  stage_name = local.env_tag
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
@@ -331,8 +332,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
 resource "aws_api_gateway_method_settings" "api-settings" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  # stage_name  = aws_api_gateway_stage.api-stage.stage_name
-  stage_name  = local.env_tag
+  stage_name  = aws_api_gateway_stage.api-stage.stage_name
   method_path = "*/*"
 
   settings {
