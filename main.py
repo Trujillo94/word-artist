dfa3e308532eda0108b92cc641f52b7af5dd594d
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-24 18:39:33 +0200

    Logging inputs.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ logger = logging.getLogger("main")
 
 
 def handler(event, context):
-    text = event['text']
-    style = event.get('style', None)
     logger.info(f'event: {event}')
     logger.info(f'context: {context}')
+    text = event['text']
+    style = event.get('style', None)
     img_url = SlackWordArtist().compute(text, style=style)
     logger.info("Successful execution")
     return img_url
