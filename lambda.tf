18691ce3a4d8dd875d2e75f801afa16eb384ce25
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-28 12:54:37 +0200

    Renamed lambda function according to specific functionality.

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 
 # Create a new Lambda function
 resource "aws_iam_role" "lambda" {
-  name               = "${local.lambda_name}-lambda-role"
+  name               = "${local.generate_lambda_name}-lambda-role"
   assume_role_policy = <<EOF
 {
    "Version": "2012-10-17",
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "lambda" {
 }
 
 resource "aws_iam_policy" "lambda" {
-  name = "${local.lambda_name}-lambda-policy"
+  name = "${local.generate_lambda_name}-lambda-policy"
   path = "/"
   # policy = data.aws_iam_policy_document.lambda.json
   policy = <<-EOF
@@ -81,7 +81,7 @@ resource "aws_iam_policy" "lambda" {
 }
 
 # resource "aws_iam_policy" "lambda-basic-execution-policy" {
-#   name   = "${local.lambda_name}-lambda-policy"
+#   name   = "${local.generate_lambda_name}-lambda-policy"
 #   path   = "/"
 #   policy = data.aws_iam_policy_document.lambda.json
 # }
@@ -96,11 +96,11 @@ resource "aws_iam_role_policy_attachment" "lambda-role-policy-attachment-lambdab
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "lambda" {
+resource "aws_lambda_function" "generate" {
   depends_on = [
     null_resource.ecr_image
   ]
-  function_name = local.lambda_name
+  function_name = local.generate_lambda_name
   role          = aws_iam_role.lambda.arn
   timeout       = 30
   image_uri     = "${aws_ecr_repository.repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
@@ -115,7 +115,7 @@ resource "aws_lambda_function" "lambda" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
+  function_name = aws_lambda_function.generate.function_name
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
