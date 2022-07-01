b9d0c012beb20b15b7a51f2fddce8df754536495
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-07-01 13:06:02 +0200

    Implemented getting user info.

--- src/wrappers/slack/slack_wrapper.py
+++ src/wrappers/slack/slack_wrapper.py
@@ -17,5 +17,15 @@ class SlackWrapper:
         return self.__client.api_test()
 
     @SlackException.error_handling
-    def send_message(self, channel_id, text, as_user=False):
-        self.__client.chat_postMessage(channel=channel_id, text=text)
+    def send_message(self, channel_id, text, username=None, icon_url=None):
+        self.__client.chat_postMessage(
+            channel=channel_id, text=text, username=username, icon_url=icon_url)
+
+    @SlackException.error_handling
+    def get_user_info(self, user_id: str) -> dict:
+        response = self.__client.users_info(user=user_id)
+        user_info = response['user']
+        if type(user_info) is dict:
+            return user_info
+        else:
+            raise Exception(f'Invalid user info. User info: <{user_info}>')
--- tests/test_slack.py
+++ tests/test_slack.py
@@ -8,12 +8,31 @@ def test_slack_bot_connection():
     assert response['ok'] is True
 
 
-def test_slack_send_message():
+def test_send_message_as_bot():
     CHANNEL_ID = load_env_var('SLACK_TESTING_CHANNEL_ID')
     text = 'Unit Testing Slack.'
-    SlackWrapper().send_message(CHANNEL_ID, text, as_user=False)
+    SlackWrapper().send_message(CHANNEL_ID, text)
+
+
+def test_get_user_name_and_icon_url_from_username():
+    user_id = 'U03HWRB6U5B'
+    ref_name = 'Oriol Trujillo'
+    user_info = SlackWrapper().get_user_info(user_id)
+    profile = user_info['profile']
+    assert profile['real_name'] == ref_name
+    assert type(profile['image_512']) is str
+
+
+def test_send_message_as_user():
+    CHANNEL_ID = load_env_var('SLACK_TESTING_CHANNEL_ID')
+    text = 'Unit Testing Slack impersonating a user.'
+    username = 'Oriol Trujillo'
+    icon_url = 'https://ca.slack-edge.com/T03HZL7AGSF-U03HWRB6U5B-decfad7279ff-512'
+    SlackWrapper().send_message(CHANNEL_ID, text, username=username, icon_url=icon_url)
 
 
 if __name__ == "__main__":
-    test_slack_bot_connection()
-    test_slack_send_message()
+    # test_slack_bot_connection()
+    # test_send_message_as_bot()
+    # test_send_message_as_user()
+    test_get_user_name_and_icon_url_from_username()
