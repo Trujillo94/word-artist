a471fc8193c27369afb3ce3e88aeb247354f6535
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-07-06 17:57:47 +0200

    Implemented all methods (untested because of AWS ban.)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 
+import requests
+
 from src.word_artist.slack_word_artist import SlackWordArtist
 from src.wrappers.slack.slack_wrapper import SlackWrapper
 
@@ -12,27 +14,37 @@ def handler(event: dict, context: dict) -> dict:
     logger.info(f'context: {context}')
     slack_msg = {}
     if 'text' in event:
-        text = event['text']
+        value = event['text']
         style = event.get('style', None)
-        slack_msg = SlackWordArtist().run(text, style=style)
+        slack_msg = SlackWordArtist().run(value, style=style)
     elif 'payload' in event:
         payload = event['payload']
         action = payload['actions'][0]
+        value = action['value']
+        response_url = payload['response_url']
         channel_id = payload['channel']['id']
-        text = action['value']
-        text = text.split('send:')[-1]
         user_id = payload['user']['id']
+        body = None
         match action['id']:
             case 'send':
-                SlackWrapper().send_message(channel_id, text, user_id=user_id)
+                # SlackWrapper().send_message(channel_id, text, user_id=user_id)
+                body = {'text': value}
             case 'cancel':
-                raise NotImplementedError
+                body = {"delete_original": True}
             case 'again':
-                raise NotImplementedError
+                style = None
+                msg = SlackWordArtist().run(value, style=style)
+                body = {
+                    "text": msg,
+                    "replace_original": True,
+                    "response_type": "ephemeral",
+                }
             case 'donate':
                 raise NotImplementedError
             case _:
                 raise NotImplementedError
+        response = requests.post(response_url, json=body)
+        logger.info(f'Slack response: {response}')
     else:
         raise Exception(f'Invalid event. Event: <{event}>')
     logger.info("Successful execution")
