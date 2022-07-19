a0dca2c1edc6ec2d5dfacc5c22f6c0e0fd2231d0
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-07-19 17:59:49 +0200

    Updated loading image.

--- src/word_artist/interactivity/slack_prompt_messages.py
+++ src/word_artist/interactivity/slack_prompt_messages.py
@@ -4,9 +4,9 @@ from src.utils.toolbox import load_env_var
 class SlackWordArtistUserMessages:
 
     loading_text: str = 'Generating a fabulous WordArt...'
-    loading_img_url: str = 'https://media0.giphy.com/media/XXH77SsudU3HW/giphy.gif?cid=790b76113f7e1b9f0e6117c95a1a3bbdde66fa6948828e6b&rid=giphy.gif&ct=g'
-    error_text = 'Oops! Something went wrong.'
-    error_img_url = 'https://media3.giphy.com/media/YDj8Ot6mIbJYs/giphy.gif?cid=ecf05e47fn4ptkyy52ocl3a3h305wyjawoa82snb48ad47br&rid=giphy.gif&ct=g'
+    loading_img_url: str = 'https://media2.giphy.com/media/RHEqKwRZDwFKE/giphy.gif?cid=ecf05e478xqsyjskmocp8bbw1b43olpd4j070sge6gzkl75y&rid=giphy.gif&ct=g'
+    error_text: str = 'Oops! Something went wrong.'
+    error_img_url: str = 'https://media3.giphy.com/media/YDj8Ot6mIbJYs/giphy.gif?cid=ecf05e47fn4ptkyy52ocl3a3h305wyjawoa82snb48ad47br&rid=giphy.gif&ct=g'
 
     def generate_loading_message(self) -> dict:
         text = self.loading_text
